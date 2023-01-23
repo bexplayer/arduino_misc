f4a23f86c8e1fc75712d4ffc15d7965eecbd1012
rebecca <122584056+hicapa@users.noreply.github.com>
2023-01-23 14:03:37 -0500

    Create button_toggle_brokendebounce.ino

---
+++ button_toggle_brokendebounce.ino
@@ -0,0 +1,35 @@
+// this is copied from my other account "eggbegg"
+// by hicapabex (2023-01-23)
+
+const int pinLed = 2;
+const int pinButton = 3;
+bool toggle = false;
+int buttonRead;
+int debounce;
+
+void setup(){
+  pinMode(pinLed, OUTPUT);
+  pinMode(pinButton, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop(){
+  if(toggle == false){
+    digitalWrite(pinLed, HIGH);
+  }
+  else{
+    digitalWrite(pinLed, LOW);
+  }
+
+  buttonRead = digitalRead(pinButton);
+  if(buttonRead == HIGH){
+    if(debounce > 20050){
+      toggle = !toggle;
+    }
+  }
+  
+  debounce++;
+  if(debounce > 20051){
+    debounce = 0;
+  }
+}
